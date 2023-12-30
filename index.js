f790890fc2b2bdcc25ae47f64c1f50c3a04594e7
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-31 00:44:25 +0300

    Create logic for the name query

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 /* This link is an endpoint for fetching charcter names */
-const API_URL = "https://rickandmortyapi.com/api/character/";
+const API_URL = "https://rickandmortyapi.com/api/character/?name=rick";
 const data = "INFO";
 
 app.use(express.static("public"));
@@ -16,33 +16,38 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.post("/", async (req, res)=>{
-  const charId = req.body.id;
-  const charName = req.body.character;
-  if(charId !== undefined && charId !== ""){
-    try{
-      const result = await axios.get(API_URL + charId);
-      const data = result.data;
-      const imgSrc = data.image;
-      res.render("index.ejs", {data:data, imgSrc:imgSrc});
-    }catch(error){
-      console.log(`Error ${error}`);
-      res.render("index.ejs", {data:"404 Error"});
-    }
-  } else if(charName !== undefined){
-    try{
-      const result = await axios.get(API_URL + `?name=${charName}`);
-      const data = result.data;
-      console.log(data);
-      const imgSrc = data.image;
-      res.render("index.ejs",{data:data, imgSrc:imgSrc});
-    }catch(error){
-      console.log(`Error ${error}`);
-      res.render("index.ejs", {data:"404 Error"});
-    }
-  }
+// app.post("/", async (req, res)=>{
+//   const charId = req.body.id;
+//   try{
+//     const result = await axios.get(API_URL + charId);
+//     const data = result.data;
+//     const imgSrc = data.image;
+//     res.render("index.ejs", {data:data, imgSrc:imgSrc});
+//   }catch(error){
+//     console.log(`Error ${error}`);
+//     res.render("index.ejs", {data:"404 Error"});
+//   }
+// })
 
+app.post("/", async (req, res)=>{
+  const charName = req.body.character;
+  try{
+    const result = await axios.get(API_URL);
+    const data = result.data.results;
+    
+    let index = Object.keys(data);
+    let chosen = Object.entries(data);
+    let arrLength = index.length;
+    const randomNumber = String(Math.floor(Math.random() * arrLength));
+    const imgSrc = data[randomNumber].image;
+    
+    res.render("index.ejs",{data:chosen[randomNumber][1], imgSrc:imgSrc});
+  }catch(error){
+    console.log(`Error ${error}`);
+    res.render("index.ejs", {data:"404 Error"});
+  }
 })
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
