171a7b22affe34a05ddcc7a8b3267a06ef0fa189
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-30 15:54:39 +0300

    Work on querying by name

--- index.js
+++ index.js
@@ -18,17 +18,31 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res)=>{
   const charId = req.body.id;
-  try{
-    const result = await axios.get(API_URL + charId);
-    const data = result.data;
-    const imgSrc = data.image;
-    res.render("index.ejs", {data:data, imgSrc:imgSrc});
-  }catch(error){
-    console.log(`Error ${error}`);
-    res.render("index.ejs", {data:"404 Error"});
+  const charName = req.body.character;
+  if(charId !== undefined && charId !== ""){
+    try{
+      const result = await axios.get(API_URL + charId);
+      const data = result.data;
+      const imgSrc = data.image;
+      res.render("index.ejs", {data:data, imgSrc:imgSrc});
+    }catch(error){
+      console.log(`Error ${error}`);
+      res.render("index.ejs", {data:"404 Error"});
+    }
+  } else if(charName !== undefined){
+    try{
+      const result = await axios.get(API_URL + `?name=${charName}`);
+      const data = result.data;
+      console.log(data);
+      const imgSrc = data.image;
+      res.render("index.ejs",{data:data, imgSrc:imgSrc});
+    }catch(error){
+      console.log(`Error ${error}`);
+      res.render("index.ejs", {data:"404 Error"});
+    }
   }
-})
 
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
