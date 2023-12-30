7797f44373a52e2c195eb252f8a52cf014257dc5
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-31 02:01:43 +0300

    Add several classes and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./css/**/*.{html,js,ejs}", "./views/*.ejs"],
   theme: {
-    extend: {fontFamily: {horror:"Patrick Hand"}},
+    extend: {fontFamily: {horror:"Patrick Hand"},
+  gridTemplateRows:{
+    layout1: "100px 150px auto",
+    layout2: "100px 1fr auto"
+  }}
   },
   plugins: [],
 };
